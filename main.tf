c749edb7f7f95cddb669917587869466b41228cc
Raanan Gavrielli <raanan.gavrielli@toluna.com>
2021-05-12 15:45:38 +0300

    next stpes

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
 locals{
-    config = jsondecode(file("config.json"))
+    config = jsondecode(file("./config.json"))
     
 }
 
 
 module "vpc" {
     source = "./modules/vpc"
-    aws_azs = slice(data.aws_availability_zones.available.names[*], 0, locals.config.number_of_azs)
-    aws_region = locals.config.aws_region
-    vpc_cidr = locals.config.vpc_cidr
-    env_name = locals.config.env_name 
-    tags = map(
-                "Name", "vpc-${locals.config.env_name}",
-                "environment", locals.config.env_name,
-                "application_role", "network",
-                "created_by", "terraform"
-                )
+    aws_azs = slice(data.aws_availability_zones.available.names[*], 0, local.config.number_of_azs)
+    aws_region = local.config.aws_region
+    vpc_cidr = local.config.vpc_cidr
+    env_name = local.config.env_name 
+    tags = tomap({
+                Name="vpc-${local.config.env_name}",
+                environment=local.config.env_name,
+                application_role="network",
+                created_by="terraform"
+    })
 }
 
 provider "aws" {
-    region = locals.config.aws_region
-    profile = locals.config.aws_profile
+    region = local.config.aws_region
+    profile = local.config.aws_profile
 }
