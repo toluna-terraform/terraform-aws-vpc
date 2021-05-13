ed70b58c939f8d4ce64a4b0f81f93770f80650c0
Raanan Gavrielli <raanan.gavrielli@toluna.com>
2021-05-13 18:41:33 +0300

    added TGW support

--- data.tf
+++ data.tf
@@ -1,1 +1,47 @@
 data "aws_availability_zones" "available" {}
+
+data "aws_ssm_parameter" "network_range" {
+    name = "/infra/network_address_range"    
+}
+data "aws_ssm_parameter" "tgw_id" {
+    name = "/infra/tgw/tgw_id"    
+}
+
+data "aws_ssm_parameter" "tgw_role" {
+    name = "/infra/tgw/tgw_role"    
+}
+
+data "aws_ssm_parameter" "tgw_region" {
+    name = "/infra/tgw/tgw_region"    
+}
+
+data "aws_ssm_parameter" "resource_share_name" {
+    name = "/infra/tgw/resource_share_name"    
+}
+
+data "aws_ssm_parameter" "route_cidr" {
+    name = "/infra/tgw/route_cidr"    
+}
+
+data "aws_ssm_parameter" "is_shared" {
+    name = "/infra/tgw/is_shared"    
+}
+
+data "aws_ec2_transit_gateway" "tgw" {
+  id        = data.aws_ssm_parameter.tgw_id.value
+}
+
+// Get Account ID
+data "aws_caller_identity" "attachment_account" {}
+
+// Get Share ARN
+data "aws_ram_resource_share" "tgw_share" {
+  resource_owner = "OTHER-ACCOUNTS"
+  name           = data.aws_ssm_parameter.resource_share_name.value
+}
+
+// Get RouteTables
+data "aws_route_tables" "route_tables" {
+  vpc_id   = module.vpc.vpc_id
+}
+
