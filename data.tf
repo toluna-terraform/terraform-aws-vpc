1f0cce03f75c9d0db8d5501d37794d160ed29a2c
Raanan Gavrielli <raanan.gavrielli@toluna.com>
2021-05-13 18:41:33 +0300

    added TGW support

--- data.tf
+++ data.tf
@@ -1,1 +1,52 @@
 data "aws_availability_zones" "available" {}
+
+data "aws_ssm_parameter" "network_range" {
+    name = "/infra/network_address_range"    
+}
+
+data "aws_ssm_parameter" "vpcs" {
+    name = "/infra/available_vpcs"    
+}
+
+data "aws_ssm_parameter" "tgw_id" {
+    name = "/infra/tgw_id"    
+}
+
+data "aws_ssm_parameter" "tgw_role" {
+    name = "/infra/tgw_role"    
+}
+
+data "aws_ssm_parameter" "tgw_region" {
+    name = "/infra/tgw_region"    
+}
+
+data "aws_ssm_parameter" "resource_share_name" {
+    name = "/infra/resource_share_name"    
+}
+
+data "aws_ssm_parameter" "route_cidr" {
+    name = "/infra/route_cidr"    
+}
+
+data "aws_ssm_parameter" "is_shared" {
+    name = "/infra/is_shared"    
+}
+
+data "aws_ec2_transit_gateway" "tgw" {
+  id        = data.aws_ssm_parameter.tgw_id.value
+}
+
+// Get Account ID
+data "aws_caller_identity" "attachment_account" {}
+
+// Get Share ARN
+data "aws_ram_resource_share" "tgw_share" {
+  resource_owner = "SELF"
+  name           = data.aws_ssm_parameter.resource_share_name.value
+}
+
+// Get RouteTables
+data "aws_route_tables" "route_tables" {
+  vpc_id   = module.vpc.vpc_id
+}
+
--- main.tf
+++ main.tf
@@ -1,22 +1,28 @@
+provider "aws" {
+    region = var.aws_region
+    profile = var.aws_profile
+}
+
 locals{
     aws_azs = slice(data.aws_availability_zones.available.names[*], 0, var.number_of_azs)
+    vpc_cidr = cidrsubnet(data.aws_ssm_parameter.network_range.value,4,var.env_index)
+    public_subnets = tolist([cidrsubnet(local.vpc_cidr,3,0),cidrsubnet(local.vpc_cidr,3,1)])
+    private_subnets = tolist([cidrsubnet(local.vpc_cidr,3,2),cidrsubnet(local.vpc_cidr,3,3)])
+    database_subnets = tolist([cidrsubnet(local.vpc_cidr,3,4),cidrsubnet(local.vpc_cidr,3,5)])
+    route_cidr = split(",",data.aws_ssm_parameter.route_cidr.value)
 }
 
 
 module "vpc" {
     source = "terraform-aws-modules/vpc/aws"
-
     name            = var.env_name
-    cidr            = var.vpc_cidr
+    cidr            = local.vpc_cidr
     azs             = local.aws_azs
-    
-
-    private_subnets = tolist(["192.168.0.0/24","192.168.1.0/24"])
-    public_subnets = tolist(["192.168.2.0/24","192.168.3.0/24"])
-
+    private_subnets = local.private_subnets
+    public_subnets = local.public_subnets
+    database_subnets = local.database_subnets
     enable_nat_gateway = true
     enable_vpn_gateway = false
-
     tags = tomap({
                 Name="vpc-${var.env_name}",
                 environment=var.env_name,
@@ -26,7 +32,44 @@ module "vpc" {
 
 }
 
-provider "aws" {
-    region = var.aws_region
-    profile = var.aws_profile
+
+// Add account id to existing TGW Share - Main Account
+resource "aws_ram_principal_association" "principal_association" {
+  count              = var.create_tgw_attachment && !data.aws_ssm_parameter.is_shared.value ? 1 : 0 
+  
+  principal          = data.aws_caller_identity.attachment_account.account_id
+  resource_share_arn = data.aws_ram_resource_share.tgw_share.id
+}
+
+// Accept the shared resource - Child Account
+resource "aws_ram_resource_share_accepter" "receiver_accept" {
+  count         = var.create_tgw_attachment && !data.aws_ssm_parameter.is_shared.value ? 1 : 0
+  depends_on    = [aws_ram_principal_association.principal_association]
+  share_arn     = aws_ram_principal_association.principal_association[0].resource_share_arn
+}
+
+// Create the VPC attachment  - Child Account
+resource "aws_ec2_transit_gateway_vpc_attachment" "vpc_attachment" {
+  depends_on = [aws_ram_principal_association.principal_association,aws_ram_resource_share_accepter.receiver_accept]
+  count              = var.create_tgw_attachment ? 1 : 0
+  subnet_ids         = local.private_subnets
+  transit_gateway_id = data.aws_ssm_parameter.tgw_id.value
+  vpc_id             = module.vpc.vpc_id
+}
+
+// Add routes  - Child Account
+resource "aws_route" "route_0" {
+  depends_on = [aws_ec2_transit_gateway_vpc_attachment.vpc_attachment]
+  for_each                    = var.create_tgw_attachment ? data.aws_route_tables.route_tables.ids : []
+  route_table_id              = each.value
+  transit_gateway_id          = data.aws_ssm_parameter.tgw_id.value
+  destination_cidr_block      = local.route_cidr[0]
+}
+// Add routes  - Child Account
+resource "aws_route" "route_1" {
+  depends_on = [aws_ec2_transit_gateway_vpc_attachment.vpc_attachment]
+  for_each                    = var.create_tgw_attachment ? data.aws_route_tables.route_tables.ids : []
+  route_table_id              = each.value
+  transit_gateway_id          = data.aws_ssm_parameter.tgw_id.value
+  destination_cidr_block      = local.route_cidr[1]
 }
