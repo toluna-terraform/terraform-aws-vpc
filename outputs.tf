4a0a7ab7bf634d748fe6dd48c1c2520ede259883
Raanan Gavrielli <raanan.gavrielli@toluna.com>
2021-07-05 18:33:16 +0300

    Improved documentation

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "attributes" {
     value = { for key, value in module.vpc : key => value }
+    description = "All the internal VPC modules output parameters"
 }
 
 output "vpce_target_group" {
     value = var.enable_private_api ? module.private_api_vpce[0].vpce_target_group : null
+    description = "Optional - VPC endpoint ENIs target group"
 }
--- variables.tf
+++ variables.tf
@@ -1,25 +1,31 @@
  variable "env_name" {
      type = string
+     description = "Environment unique identifier"
  }
  variable "number_of_azs" {
      type = number
      default = 2
+     description = "Number of Availability Zones to setup"
  }
  variable "env_type" {
      type = string
      default = "non-prod"
+     description = "Environment Cardinality: prod or non-prod"
  }
 
  variable "env_index" {
      type = number
+     description = "The VPC CIDR subnet index, extracted from the whole available network address range of the account."
  }
 
  variable "create_tgw_attachment"{
      type = bool
      default = false
+     description = "Attach a Transite Gateway to the VPC and update related route tables"
  }
 
  variable "enable_private_api"{
      type = bool
      default = false
+     description = "For SAM integration - Create VPC endpoint and required resources and export them as SSM params"
  }
