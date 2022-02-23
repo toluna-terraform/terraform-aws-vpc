edbb0274ad0a8c1d51ad78bac643a51f04ddf19d
Evgeny Gigi <evgeny.gigi@haievgenygmac1.local>
2022-02-23 11:30:10 +0200

    adding nat-instance module

---
+++ modules/nat-instance/data.tf
@@ -0,0 +1,41 @@
+// Get VPC data.
+data "aws_vpc" "current_vpc" {
+    id = var.aws_vpc_id
+}
+
+// Choosing the image for NAT Instance.
+data "aws_ami" "amazon_linux" {
+most_recent = true
+  filter {
+    name   = "name"
+    values = [var.amazon_ec2_linux_image]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = [var.amazon_ec2_instance_virtualization_type]
+  }
+  owners = [var.amazon_owner_id]
+}
+
+// Script to setup the NAT Instance.
+data "template_file" "nat_instance_setup_template" {
+  template = file("${path.module}/nat-instance-setup.sh.tpl")
+  vars = {
+  }
+  // Vars maybe used in the future.
+}
+
+// Get NAT Instance data.
+data "aws_instance" "nat_instance_data" {
+  instance_id = aws_instance.nat_instance.id
+}
+
+// Get route tables of private networks of current environment.
+data "aws_route_tables" "route_tables_of_private_networks" {
+  vpc_id   = var.aws_vpc_id
+    
+    filter {
+    name   = "tag:Name"
+    values = ["${var.env_name}-private*"]
+  }
+}
