69071f8664f3862709c43ed92ca2c5f5e9941398
SrinivasaRao A <srinivasarao.asomayajula@toluna.com>
2023-03-14 19:20:00 +0530

    fixed target group name length

--- modules/private-api-gw/main.tf
+++ modules/private-api-gw/main.tf
@@ -25,7 +25,7 @@ resource "aws_vpc_endpoint" "execute_api" {
 }
 
 resource "aws_lb_target_group" "tg_vpce" {
-  name        = "tg-${var.name_suffix}-api-gw"
+  name        = "tg-${terraform.workspace}-api-gw"
   port        = 443
   protocol    = "HTTPS"
   target_type = "ip"
